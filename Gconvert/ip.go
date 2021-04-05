1de2b2402939593d75dd17009a1810cb0a0d1ac6
code <codescan@qq.com>
2021-04-05 11:21:55 +0800

    update

---
+++ Gconvert/ip.go
@@ -0,0 +1,40 @@
+package Gconvert
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/code-scan/Goal/Ghttp"
+	"log"
+)
+
+type IPinfoResult struct {
+	IP       string `json:"ip"`
+	City     string `json:"city"`
+	Region   string `json:"region"`
+	Country  string `json:"country"`
+	Loc      string `json:"loc"`
+	Org      string `json:"org"`
+	Postal   string `json:"postal"`
+	Timezone string `json:"timezone"`
+	Readme   string `json:"readme"`
+}
+
+func IPinfo(ip string, token string) IPinfoResult {
+	var result IPinfoResult
+	http := Ghttp.Http{}
+	http.New("GET", fmt.Sprintf("https://ipinfo.io/%s/json", ip))
+	if token != "" {
+		http.SetHeader("Authorization", fmt.Sprintf("Bearer %s", token))
+	}
+	http.Execute()
+	ret, err := http.Byte()
+	if err != nil {
+		log.Println("[!] IPinfo Error: ", err)
+		return result
+	}
+	if err = json.Unmarshal(ret, &result); err != nil {
+		log.Println("[!] IPinfo Unmarshal Error: ", err)
+	}
+	return result
+
+}
