f9a23b8a4018734e1610cf598fe962d554e89e40
code <codescan@qq.com>
2021-04-19 10:37:53 +0800

    增加Gsql模块

---
+++ Glogin/sql.go
@@ -0,0 +1,98 @@
+package Glogin
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"strings"
+)
+
+func login(host, port, username, password, type_ string) (*gorm.DB, bool) {
+	var connStr string
+	switch type_ {
+	case "mysql":
+		connStr = fmt.Sprintf("%s:%s@tcp(%s:%s)/?timeout=%ds", username, password, host, port, 10)
+	case "postgres":
+		connStr = fmt.Sprintf("host=%s port=%s user=%s dbname=test sslmode=disable password=%s timeout=%ds", host, port, username, password, 10)
+	case "mssql":
+		connStr = fmt.Sprintf("server=%s;user id=%s;password=%s;port=%s;database=test;timeout=%ds", host, username, password, port, 10)
+	}
+	db, err := gorm.Open(type_, connStr)
+	if err != nil {
+		return nil, false
+	}
+	return db, true
+}
+func SqlLogin(host, port, username, password, type_ string) bool {
+	db, isLogin := login(host, port, username, password, type_)
+	if isLogin {
+		defer db.Close()
+		return true
+	}
+	return false
+}
+func SqlQuery(host, port, username, password, type_, sql string) []interface{} {
+	var result []interface{}
+
+	db, isLogin := login(host, port, username, password, type_)
+	if isLogin == false {
+		return result
+	}
+	isRaw := false
+	raw := []string{
+		"update ",
+		"insert ",
+		"delete ",
+		"drop ",
+		"create",
+		"set ",
+		"exec ",
+		"execute ",
+	}
+	for _, r := range raw {
+		if strings.Contains(sql, r) {
+			isRaw = true
+			break
+		}
+	}
+	if isRaw {
+		db.Exec(sql)
+		return result
+	}
+
+	rows, err := db.Raw(sql).Rows()
+	if err != nil {
+		result = append(result, err.Error())
+		return result
+	}
+	var colums []string
+	//result := make(map[]interface{})
+	for rows.Next() {
+
+		if colums == nil {
+			colums, _ = rows.Columns()
+		}
+		result = append(result, colums)
+		columns := make([]interface{}, len(colums))
+		columnPointers := make([]interface{}, len(colums))
+		for i, _ := range columns {
+			columnPointers[i] = &columns[i]
+		}
+		rows.Scan(columnPointers...)
+		//m := make(map[string]interface{})
+		m := make(map[string]interface{})
+		for i, colName := range colums {
+			val := columnPointers[i].(*interface{})
+			m[colName] = *val
+		}
+
+		//log.Println(m)
+		result = append(result, m)
+		//result = append(result, row)
+	}
+	return result
+	//log.Println(result)
+
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/armon/go-socks5 v0.0.0-20160902184237-e75332964ef5
 	github.com/hashicorp/yamux v0.0.0-20210316155119-a95892c5f864
+	github.com/jinzhu/gorm v1.9.16
 	github.com/jlaffaye/ftp v0.0.0-20210307004419-5d4190119067
 	github.com/projectdiscovery/wappalyzergo v0.0.2
 	github.com/taknb2nch/go-pop3 v0.0.0-20140407022049-72f6fae12d45
