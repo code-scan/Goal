229e144888ea6159ac8e69320005d71428b7b3cc
c <cond0r@126.com>
2021-05-21 16:06:48 +0800

    优化代码

--- Ghttp/response.go
+++ Ghttp/response.go
@@ -65,6 +65,12 @@ func (h *Http) readAll() ([]byte, error) {
 			buffer = nil
 		}
 	}()
+	if h.HttpResponse == nil {
+		return nil, fmt.Errorf("HttpResponse is nil")
+	}
+	if h.HttpResponse.Body == nil {
+		return nil, fmt.Errorf("HttpResponse.Body is nil")
+	}
 	_, err := io.Copy(buffer, h.HttpResponse.Body)
 	if err != nil && err != io.EOF {
 		//log.Printf("adapter io.copy failure error:%v \n", err)
@@ -76,44 +82,26 @@ func (h *Http) readAll() ([]byte, error) {
 
 // string的返回值
 func (h *Http) Text() (string, error) {
-	var result []byte
-	var err error
 	defer func() {
 		if r := recover(); r != nil {
 			log.Println("Recovered in Text : ", r)
 		}
 	}()
-	if h.HttpResponse == nil {
+	r, err := h.readAll()
+	if err != nil {
 		return "", err
 	}
-	if h.HttpResponse.Body == nil {
-		return "", err
-	}
-	if result, err = h.readAll(); err != nil {
-		log.Println(err)
-	}
-	return string(result), err
+	return string(r), err
 }
 
 // byte的返回值
 func (h *Http) Byte() ([]byte, error) {
-	var result []byte
-	var err error
 	defer func() {
 		if r := recover(); r != nil {
 			log.Println("Recovered in Byte : ", r)
 		}
 	}()
-	if h.HttpResponse == nil {
-		return result, err
-	}
-	if h.HttpResponse.Body == nil {
-		return result, err
-	}
-	if result, err = h.readAll(); err != nil {
-		log.Println(err)
-	}
-	return result, err
+	return h.readAll()
 }
 func (h *Http) SaveToFile(file string) (bool, error) {
 	var err error
@@ -124,12 +112,15 @@ func (h *Http) SaveToFile(file string) (bool, error) {
 	}()
 	if h.HttpResponse == nil {
 		log.Println("[!] HttpResponse Is Closed")
-		return false, err
+		return false, fmt.Errorf("HttpResponse is nil")
 	}
 	var f *os.File
 	f, err = os.Create(file)
-	if h.HttpResponse.Body != nil && err == nil {
-		defer h.HttpResponse.Body.Close()
+	if h.HttpResponse.Body == nil {
+		return false, fmt.Errorf("HttpResponse.Body is nil")
+	}
+	defer h.HttpResponse.Body.Close()
+	if err == nil {
 		defer f.Close()
 		_, err = io.Copy(f, h.HttpResponse.Body)
 	}
